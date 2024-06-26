766cd862dfbf47ff3f64e78267236050afd5edd9
HansakaHeli <programminghhr66@gmail.com>
2024-06-27 01:50:30 +0530

    Create endpoint to upload image

--- Backend/routers/post.py
+++ Backend/routers/post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends,status
+from fastapi import APIRouter, Depends,status, UploadFile,File
 from sqlalchemy.orm import Session
 from routers.schemas import PostBase, PostDisplay
 from db.database import get_db
@@ -6,6 +6,7 @@ from fastapi.exceptions import HTTPException
 from db.database import get_db
 from db import db_post
 from typing import List
+import random, string, shutil
 
 router = APIRouter(
     prefix='/post',
@@ -24,3 +25,16 @@ def create(request: PostBase, db: Session = Depends(get_db)):
 @router.get("/all", response_model=List[PostDisplay])
 def posts(db: Session = Depends(get_db)):
     return db_post.get_all(db)
+
+@router.post('/image')
+def upload_image(image:UploadFile=File(...)):
+    letters = string.ascii_letters
+    rand_str = ''.join(random.choice(letters) for i in range(6))
+    new = f'_{rand_str}.'
+    filename = new.join(image.filename.rsplit('.', 1))
+    path = f'images/{filename}'
+
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {'filename': path}
