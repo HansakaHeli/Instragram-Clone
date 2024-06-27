1045f53b83ea07e17b27a9f1874cc1289f5f5608
HansakaHeli <programminghhr66@gmail.com>
2024-06-27 20:55:17 +0530

    Add endpoint for comment

--- Backend/routers/schemas.py
+++ Backend/routers/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import List
 
 class UserBase(BaseModel):
     username: str
@@ -24,6 +25,15 @@ class User(BaseModel):
   class Config():
     orm_mode = True
 
+# For PostDisplay
+class Comment(BaseModel):
+  text: str
+  username: str
+  timestamp: datetime
+  class Config():
+    orm_mode = True
+
+
 class PostDisplay(BaseModel):
   id: int
   image_url: str
@@ -31,7 +41,7 @@ class PostDisplay(BaseModel):
   caption: str
   timestamp: datetime
   user: User
-  # comments: List[Comment]
+  comments: List[Comment]
   class Config():
     orm_mode = True
 
@@ -39,3 +49,8 @@ class UserAuth(BaseModel):
   id: int
   username: str
   email: str
+
+class CommentBase(BaseModel):
+  username: str
+  text: str
+  post_id: int
